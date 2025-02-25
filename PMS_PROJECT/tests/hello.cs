b159a698b2482baa626e068091a3b77841111ac2
Pedro Quix <83504805+pedro11065@users.noreply.github.com>
2025-02-25 12:05:14 -0300

    Add project files.

---
+++ PMS_PROJECT/tests/hello.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+class Hello
+{
+    static void Main()
+    {
+        string nome = "";
+        Console.WriteLine("vou adivinhar seu nome, para começar digite seu nome: ");
+
+        nome = Console.ReadLine();
+
+        Console.WriteLine("seu nome é:" + nome);
+
+
+
+    }
+}
+
